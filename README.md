4e32b596977f9222bb5e2f3d4714165316662557
Dmitry Melnikov <d.dash.mv@gmail.com>
2021-12-04 18:30:18 +0200

    chore: stash work

---
+++ README.md
@@ -0,0 +1,1 @@
+DATABASE_URL=blog_db.sqlt
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv::dotenv;
+use std::env;
+use blog_actix::Blog;
+
+fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
+    env::set_var("RUST_LOG","actix_web=info");
+    env_logger::init();
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let app = Blog::new(8998);
+    app.run(database_url)
 }
